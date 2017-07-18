e4028a0a621261d658c9e5e9410c6f12f9f4cd6c
Thiru <thirunavukkarasu@outlook.com>
2017-07-18 23:52:24 +0530

    Mail Sent Successfully

--- Vip2Vip/Controllers/HomeController.cs
+++ Vip2Vip/Controllers/HomeController.cs
@@ -30,38 +30,38 @@ namespace Vip2Vip.Controllers
             //    periopContentViewModelList.Add(periopContentViewModel);
             //}
 
-            string fromAddress = "thiru@shreeharifoods.net";
-            string mailPassword = "testing123*";       
+            string fromAddress = "thirushr@gmail.com";
+            string mailPassword = "9444932329";
 
-            //StringBuilder htmlbody = new StringBuilder();
-            //htmlbody.Append("<table>");
-            //htmlbody.Append("<tr><th colspan=3><h2> New User contact Details</h2></th></tr>");
-            //htmlbody.Append("<tr><td>FirstName</td> <td>:</td><td>");
-            //htmlbody.Append("Thiru");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("<tr><td>LastName</td> <td>:</td><td>");
-            //htmlbody.Append("Perumal");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("<tr><td>Area</td> <td>:</td><td>");
-            //htmlbody.Append("Chennai");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("<tr><td>Email Address</td> <td>:</td><td>");
-            //htmlbody.Append("test@gmail.com");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("<tr><td>Contact Number</td> <td>:</td><td>");
-            //htmlbody.Append("84575 23568");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("<tr><td>User Message</td> <td>:</td><td>");
-            //htmlbody.Append("Test");
-            //htmlbody.Append("</td></tr>");
-            //htmlbody.Append("</table>");
+            StringBuilder htmlbody = new StringBuilder();
+            htmlbody.Append("<table>");
+            htmlbody.Append("<tr><th colspan=3><h2> New User contact Details</h2></th></tr>");
+            htmlbody.Append("<tr><td>FirstName</td> <td>:</td><td>");
+            htmlbody.Append("Thiru");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("<tr><td>LastName</td> <td>:</td><td>");
+            htmlbody.Append("Perumal");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("<tr><td>Area</td> <td>:</td><td>");
+            htmlbody.Append("Chennai");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("<tr><td>Email Address</td> <td>:</td><td>");
+            htmlbody.Append("test@gmail.com");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("<tr><td>Contact Number</td> <td>:</td><td>");
+            htmlbody.Append("84575 23568");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("<tr><td>User Message</td> <td>:</td><td>");
+            htmlbody.Append("Test");
+            htmlbody.Append("</td></tr>");
+            htmlbody.Append("</table>");
 
             // Create smtp connection.
             SmtpClient client = new SmtpClient();
-            client.Port = 25;//outgoing port for the mail.
-            client.Host = "webmail.shreeharifoods.net";
-            client.EnableSsl = false;
-            client.Timeout = 1000;
+            client.Port = 587;//outgoing port for the mail.
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 100000;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
             client.Credentials = new System.Net.NetworkCredential(fromAddress, mailPassword);
@@ -72,39 +72,15 @@ namespace Vip2Vip.Controllers
 
             send_mail.IsBodyHtml = true;
             //address from where mail will be sent.
-            send_mail.From = new MailAddress("thiru@shreeharifoods.net");
+            send_mail.From = new MailAddress("thirushr@gmail.com");
             //address to which mail will be sent.           
-            send_mail.To.Add(new MailAddress("thirushr@gmail.com"));
+            send_mail.To.Add(new MailAddress("thirunavukkarasu@outlook.com"));
             //subject of the mail.
             send_mail.Subject = "Test Mail VIP 2 VIP";
 
-            send_mail.Body = "Test"; //htmlbody.ToString();
+            send_mail.Body = htmlbody.ToString();
             client.Send(send_mail);
-
-            //var fromAddress = new MailAddress("thirushr@gmail.com", "Thiru Perumal");
-            //var toAddress = new MailAddress("thirunavukkarasu.p@infosys.com", "Thiru");
-            //const string fromPassword = "9444932329";
-            //const string subject = "Test Mail VIP 2 VIP";
-            //const string body = "Test";
-
-            //var smtp = new SmtpClient
-            //{
-            //    Host = "smtp.gmail.com",
-            //    Port = 587,
-            //    EnableSsl = true,
-            //    DeliveryMethod = SmtpDeliveryMethod.Network,
-            //    UseDefaultCredentials = false,
-            //    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            //};
-            //using (var message = new MailMessage(fromAddress, toAddress)
-            //{
-            //    Subject = subject,
-            //    Body = body
-            //})
-            //{
-            //    smtp.Send(message);
-            //}
-
+            
             ViewBag.Status = "Success";
 
             return View(periopContentViewModelList);
